8b804448e72e9f78cdb8c29569a4e9fe578ec00c
lax <czjordan323@gmail.com>
2019-07-09 01:08:24 -0700

    Computer choice added and correct link to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,3 +2,14 @@ var winCount = 1;
 var lossesCount =  0;
 var guessesRemaining = 9;
 var failedAttempts = [];
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
+,"r","s","t","u","v","w","x","y","z"];
+
+
+// computer pick
+var randomPick = Math.floor(Math.random() * letters.length);
+var computerPick = letters[randomPick];
+
+console.log(computerPick);
+
+
